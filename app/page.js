74fb9b272b66a6b65923164fc8ec5b577bc0e04e
ritsu-me <me@ritsu-me.com>
2023-09-10 11:49:21 +0900

    aaa

---
+++ app/page.js
@@ -0,0 +1,20 @@
+export default function Home() {
+  return (
+    <>
+      <div className="grid grid-cols-12 gap-12">
+        <div className="sticky bg-indigo-600 height-screen">1</div>
+        <div className="sticky bg-indigo-600 height-screen">2</div>
+        <div className="sticky bg-indigo-600 height-screen">3</div>
+        <div className="sticky bg-indigo-600 height-screen">4</div>
+        <div className="sticky bg-indigo-600 height-screen">5</div>
+        <div className="sticky bg-indigo-600 height-screen">6</div>
+        <div className="sticky bg-indigo-600 height-screen">7</div>
+        <div className="sticky bg-indigo-600 height-screen">8</div>
+        <div className="sticky bg-indigo-600 height-screen">9</div>
+        <div className="sticky bg-indigo-600 height-screen">10</div>
+        <div className="sticky bg-indigo-600 height-screen">11</div>
+        <div className="sticky bg-indigo-600 height-screen">12</div>
+      </div>
+    </>
+  );
+}
